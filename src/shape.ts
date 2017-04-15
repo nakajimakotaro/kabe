df35321f01c4df980ccaa7777f206da87e7aa8a1
nakajimakotaro <harusann2@gmail.com>
2017-04-15 09:55:40 +0900

    アクセサを追加

--- src/shape.ts
+++ src/shape.ts
@@ -2,28 +2,40 @@ export class Shape{
     collisionList: {owner: Object|null, shape:Shape}[] = [];
     owner: Object|null = null;
     constructor(
-        public x:number,
-        public y:number
+        public x:number = 0,
+        public y:number = 0
     ){
     }
 }
 export class Rectangle extends Shape{
     constructor(
-        public x:number,
-        public y:number,
-        public width:number,
-        public height:number,
+        public x:number = 0,
+        public y:number = 0,
+        public width:number = 0,
+        public height:number = 0,
         public angle:number = 0,
         public name:string = ""
     ){
         super(x, y);
     }
+    left(){
+        return this.x - this.width / 2;
+    }
+    right(){
+        return this.x + this.width / 2;
+    }
+    top(){
+        return this.y - this.height / 2;
+    }
+    bottom(){
+        return this.y + this.height / 2;
+    }
 }
 export class Circle extends Shape{
     constructor(
-    public x:number,
-    public y:number,
-    public r:number,
+    public x:number = 0,
+    public y:number = 0,
+    public r:number = 0,
     public angle:number = 0,
     public name:string = ""
     ){
@@ -33,8 +45,8 @@ export class Circle extends Shape{
 
 export class Point extends Shape{
     constructor(
-        public x:number,
-        public y:number,
+        public x:number = 0,
+        public y:number = 0,
         public angle:number = 0,
         public name:string = ""
     ){
