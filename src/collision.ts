bf2027700502dca3ca39f42994c34fd9075a9689
nakajimakotaro <harusann2@gmail.com>
2017-05-17 22:47:58 +0900

    分割して当たり判定を取るようにした

--- src/collision.ts
+++ src/collision.ts
@@ -1,13 +1,13 @@
-import {Shape,Rectangle,Circle,Point} from "./shape";
+import { Shape, Rectangle, Circle, Point } from "./shape";
 import _ = require('lodash');
-import {Game} from "./script";
+import { Game } from "./script";
 
 class CollisionAreaNode {
-    child:CollisionAreaNode[] = new Array(4);
-    inObject:{object:CollisionObject, areaNode:CollisionAreaNode}[] = [];
-    constructor(public index:number, public shape:Rectangle){
+    child: CollisionAreaNode[] = new Array(4);
+    inObject: { object: CollisionObject, areaNode: CollisionAreaNode }[] = [];
+    constructor(public index: number, public shape: Rectangle) {
     }
-    isEnd(){
+    isEnd() {
         return (
             this.child[0] === undefined &&
             this.child[1] === undefined &&
@@ -15,49 +15,68 @@ class CollisionAreaNode {
             this.child[3] === undefined);
     }
 }
-interface CollisionObject{
-    owner:Object;
-    shape:Shape;
+interface CollisionObject {
+    owner: Object;
+    shape: Shape;
 }
-export class Collision{
+export class Collision {
     list: CollisionObject[] = [];
 
-    constructor(public game:Game){
+    constructor(public game: Game) {
     }
-    add(owner:Object, shape:Shape){
-        this.list.push({shape: shape, owner: owner});
+    add(owner: Object, shape: Shape) {
+        this.list.push({ shape: shape, owner: owner });
     }
-    remove(owner:Object, shape:Shape){
-        _.remove(this.list, (v)=>{
+    remove(owner: Object, shape: Shape) {
+        _.remove(this.list, (v) => {
             return (
                 v.owner === owner &&
-                    v.shape === shape);
+                v.shape === shape);
         });
     }
-    collision(shape:Shape){
+    collision(shape: Shape) {
         shape.collisionList = [];
-        for(let a of this.list){
-            if(Collision.intersects(shape, a.shape)){
+        for (let a of this.list) {
+            if (Collision.intersects(shape, a.shape)) {
                 shape.collisionList.push(a);
             }
         }
     }
-    tick(){
-        for(let collision of this.list){
+    tick() {
+        for (let collision of this.list) {
             collision.shape.collisionList = [];
         }
         let root = new CollisionAreaNode(0, this.game.level.shape.clone());
-        root = this.treeGenerate(root, this.list);
-        Collision.roundTrip(root.child[0].inObject.map((e)=>e.object));
-        Collision.roundTrip(root.child[1].inObject.map((e)=>e.object));
-        Collision.roundTrip(root.child[2].inObject.map((e)=>e.object));
-        Collision.roundTrip(root.child[3].inObject.map((e)=>e.object));
+        Collision.treeGenerate(root, this.list, 20, this.game);
+        Collision.treeProcess(root);
 
-        for(let collision of this.list){
+        for (let collision of this.list) {
             collision.shape.collisionList = _.uniq(collision.shape.collisionList);
         }
     }
-    private treeGenerate(root:CollisionAreaNode, list:CollisionObject[]){
+    private static treeProcess(tree: CollisionAreaNode, collisionStack: CollisionObject[] = []) {
+        if (tree.isEnd()) {
+            return;
+        }
+
+        for (let node of tree.child) {
+            Collision.roundTrip(node.inObject.map((e) => e.object));
+            Collision.roundTrip2(collisionStack, node.inObject.map((e) => e.object));
+        }
+        for (let node of tree.child) {
+            collisionStack = collisionStack.concat(node.inObject.map((e) => e.object));
+        }
+        for (let node of tree.child) {
+            Collision.treeProcess(node, collisionStack);
+        }
+        for (let node of tree.child) {
+            collisionStack.length = collisionStack.length - node.inObject.length;
+        }
+    }
+    private static treeGenerate(root: CollisionAreaNode, list: CollisionObject[], count: number, game: Game) {
+        if (count == 0) {
+            return;
+        }
         let areaNode = root;
         areaNode.child[0] = new CollisionAreaNode(1,
             new Rectangle(
@@ -65,51 +84,68 @@ export class Collision{
                 areaNode.shape.top() + (areaNode.shape.height / 4),
                 areaNode.shape.width / 2,
                 areaNode.shape.height / 2));
-        areaNode.child[1] = new CollisionAreaNode(2, 
+        areaNode.child[1] = new CollisionAreaNode(2,
             new Rectangle(
                 areaNode.shape.x + (areaNode.shape.width / 4),
                 areaNode.shape.top() + (areaNode.shape.height / 4),
                 areaNode.shape.width / 2,
                 areaNode.shape.height / 2));
-        areaNode.child[2] = new CollisionAreaNode(3, 
+        areaNode.child[2] = new CollisionAreaNode(3,
             new Rectangle(
                 areaNode.shape.left() + (areaNode.shape.width / 4),
                 areaNode.shape.y + (areaNode.shape.height / 4),
                 areaNode.shape.width / 2,
                 areaNode.shape.height / 2));
-        areaNode.child[3] = new CollisionAreaNode(4, 
+        areaNode.child[3] = new CollisionAreaNode(4,
             new Rectangle(
                 areaNode.shape.x + (areaNode.shape.width / 4),
                 areaNode.shape.y + (areaNode.shape.height / 4),
                 areaNode.shape.width / 2,
                 areaNode.shape.height / 2));
         let debug = true;
-        if(debug){
-            let color = [ 0xf44336, 0xE91E63, 0x9C27B0, 0x673AB7, 0x3F51B5, 0x2196F3, 0x03A9F4, 0x00BCD4, 0x009688, 0x4CAF50, 0x8BC34A, 0xCDDC39, 0xFFEB3B, 0xFFC107, 0xFF9800, 0xFF5722 ]; let count = 0; for(let areaNodeChild of areaNode.child){ count++; if(count == color.length){ count = 0; } this.game.level.view .beginFill(color[count], 0.3) .drawRect(areaNodeChild.shape.left(), areaNodeChild.shape.top(), areaNodeChild.shape.width, areaNodeChild.shape.height) .endFill(); count++; }
+        if (debug) {
+            let color = [0xf44336, 0xE91E63, 0x9C27B0, 0x673AB7, 0x3F51B5, 0x2196F3, 0x03A9F4, 0x00BCD4, 0x009688, 0x4CAF50, 0x8BC34A, 0xCDDC39, 0xFFEB3B, 0xFFC107, 0xFF9800, 0xFF5722];
+            let count = 0;
+            for (let areaNodeChild of areaNode.child) {
+                count++;
+                if (count == color.length) {
+                    count = 0;
+                }
+                game.level.view
+                    .beginFill(color[count], 0.3)
+                    .drawRect(areaNodeChild.shape.left(), areaNodeChild.shape.top(), areaNodeChild.shape.width, areaNodeChild.shape.height)
+                    .endFill();
+                count++;
+            }
         }
-        let nextNodeChild = new Map<CollisionAreaNode, {object:CollisionObject, areaNode:CollisionAreaNode}[]>();
+        let nextNodeChild = new Map<CollisionAreaNode, CollisionObject[]>();
         nextNodeChild.set(areaNode.child[0], []);
         nextNodeChild.set(areaNode.child[1], []);
         nextNodeChild.set(areaNode.child[2], []);
         nextNodeChild.set(areaNode.child[3], []);
-        for(let object of list){
-            let onlyInNode:CollisionAreaNode|null = null;
-            for(let areaNodeChild of areaNode.child){
-                if(Collision.intersects(areaNodeChild.shape, object.shape)){
-                    if(onlyInNode == null){
+        for (let object of list) {
+            let onlyInNode: CollisionAreaNode | null = null;
+            for (let areaNodeChild of areaNode.child) {
+                if (Collision.intersects(areaNodeChild.shape, object.shape)) {
+                    if (onlyInNode == null) {
                         onlyInNode = areaNodeChild;
-                    }else{
+                    } else {
                         onlyInNode = null;
-                        areaNodeChild.inObject.push({object: object, areaNode: areaNodeChild});
+                        areaNodeChild.inObject.push({ object: object, areaNode: areaNodeChild });
                         break;
                     }
                 }
             }
-            if(onlyInNode != null){
-                onlyInNode.inObject.push({object: object, areaNode: onlyInNode});
-                //nextNodeChild.get(onlyInNode)!.push({object:object, areaNode: onlyInNode});
+            if (onlyInNode != null) {
+                nextNodeChild.get(onlyInNode)!.push(object);
             }
         }
+        for (let [areaNode, objectList] of nextNodeChild) {
+            if (objectList.length != 0) {
+                Collision.treeGenerate(areaNode, objectList, count - 1, game);
+            }
+        }
+
         //再帰呼出し
         return areaNode;
     }
@@ -145,75 +181,94 @@ export class Collision{
     // }
     // positioningNode(root, list);
     // }
-    private static roundTrip(collisionList:CollisionObject[]){
-        for(let x = 0; x < collisionList.length; x++){
+    //総当り
+    private static roundTrip(collisionList: CollisionObject[]) {
+        if (!Array.isArray(collisionList)){
+            return;
+        }
+        for (let x = 0; x < collisionList.length; x++) {
             const a = collisionList[x];
-            for(let y = x + 1; y < collisionList.length; y++){
+            for (let y = x + 1; y < collisionList.length; y++) {
                 const b = collisionList[y];
-                if(Collision.intersects(a.shape, b.shape)){
+                if (Collision.intersects(a.shape, b.shape)) {
                     a.shape.collisionList.push(b);
                     b.shape.collisionList.push(a);
                 }
             }
         }
     }
-    static intersects(a:Shape, b:Shape):boolean{
-        if(a instanceof Rectangle       && b instanceof Rectangle){
+    //AとBの総当り
+    //Aの要素同士では当たり判定を取らない(Bも同じ)
+    private static roundTrip2(collisionListA: CollisionObject[], collisionListB: CollisionObject[]) {
+        if (!Array.isArray(collisionListA) || !Array.isArray(collisionListB)){
+            return;
+        }
+        for (let a of collisionListA) {
+            for (let b of collisionListB) {
+                if (Collision.intersects(a.shape, b.shape)) {
+                    a.shape.collisionList.push(b);
+                    b.shape.collisionList.push(a);
+                }
+            }
+        }
+    }
+    static intersects(a: Shape, b: Shape): boolean {
+        if (a instanceof Rectangle && b instanceof Rectangle) {
             return Collision.RectRect(a, b);
-        }else if(a instanceof Circle    && b instanceof Circle){
+        } else if (a instanceof Circle && b instanceof Circle) {
             return Collision.CircleCircle(a, b);
-        }else if(a instanceof Rectangle && b instanceof Circle){
+        } else if (a instanceof Rectangle && b instanceof Circle) {
             return Collision.RectCircle(a, b);
-        }else if(a instanceof Circle    && b instanceof Rectangle){
+        } else if (a instanceof Circle && b instanceof Rectangle) {
             return Collision.RectCircle(b, a);
-        }else if(a instanceof Point     && b instanceof Rectangle){
+        } else if (a instanceof Point && b instanceof Rectangle) {
             return Collision.PointRectangle(a, b);
-        }else if(a instanceof Rectangle && b instanceof Point){
+        } else if (a instanceof Rectangle && b instanceof Point) {
             return Collision.PointRectangle(b, a);
-        }else if(a instanceof Point     && b instanceof Circle){
+        } else if (a instanceof Point && b instanceof Circle) {
             return Collision.PointCircle(a, b);
-        }else if(a instanceof Circle    && b instanceof Point){
+        } else if (a instanceof Circle && b instanceof Point) {
             return Collision.PointCircle(b, a);
-        }else if(a instanceof Point     && b instanceof Point){
+        } else if (a instanceof Point && b instanceof Point) {
             return Collision.PointPoint(a, b);
-        }else{
+        } else {
             return true;
         }
     }
 
-    static CircleCircle(a:Circle, b:Circle):boolean{
+    static CircleCircle(a: Circle, b: Circle): boolean {
         return Math.hypot(a.x - b.x, a.y - b.y) < a.r + b.r;
     }
-    static RectRect(a:Rectangle, b:Rectangle):boolean{
+    static RectRect(a: Rectangle, b: Rectangle): boolean {
         //TODO angle対応
         return (
-            (a.left() < b.right() ) &&
-            (a.right()  > b.left()) &&
+            (a.left() < b.right()) &&
+            (a.right() > b.left()) &&
             (a.top() < b.bottom()) &&
             (a.bottom() > b.top()));
     }
-    static RectCircle(rect:Rectangle, cicle:Circle):boolean{
+    static RectCircle(rect: Rectangle, cicle: Circle): boolean {
         //TODO angle対応
         return (
-            rect.left() - cicle.r < cicle.x && rect.right()   + cicle.r > cicle.x &&
-            rect.top()  - cicle.r < cicle.y && rect.bottom()  + cicle.r > cicle.y
+            rect.left() - cicle.r < cicle.x && rect.right() + cicle.r > cicle.x &&
+            rect.top() - cicle.r < cicle.y && rect.bottom() + cicle.r > cicle.y
         ) ||
-        (
-            Math.hypot(rect.left()  - cicle.x, rect.top()    - cicle.y) < cicle.r ||
-            Math.hypot(rect.left()  - cicle.x, rect.bottom() - cicle.y) < cicle.r ||
-            Math.hypot(rect.right() - cicle.x, rect.top()    - cicle.y) < cicle.r ||
-            Math.hypot(rect.right() - cicle.x, rect.bottom() - cicle.y) < cicle.r
-        );
+            (
+                Math.hypot(rect.left() - cicle.x, rect.top() - cicle.y) < cicle.r ||
+                Math.hypot(rect.left() - cicle.x, rect.bottom() - cicle.y) < cicle.r ||
+                Math.hypot(rect.right() - cicle.x, rect.top() - cicle.y) < cicle.r ||
+                Math.hypot(rect.right() - cicle.x, rect.bottom() - cicle.y) < cicle.r
+            );
     }
-    static PointPoint(a:Point, b:Point):boolean{
+    static PointPoint(a: Point, b: Point): boolean {
         return a.x == b.x && a.y == b.y;
     }
-    static PointRectangle(point:Point, rect:Rectangle):boolean{
+    static PointRectangle(point: Point, rect: Rectangle): boolean {
         //TODO angle対応
-        return (rect.left() < point.x) && (rect.right()  > point.x) && 
-        (rect.top() < point.y) && (rect.bottom() > point.y); 
+        return (rect.left() < point.x) && (rect.right() > point.x) &&
+            (rect.top() < point.y) && (rect.bottom() > point.y);
     }
-    static PointCircle(point:Point, cicle:Circle):boolean{
+    static PointCircle(point: Point, cicle: Circle): boolean {
         return Math.hypot(cicle.x - point.x, cicle.y - point.y) < cicle.r;
     }
 }
