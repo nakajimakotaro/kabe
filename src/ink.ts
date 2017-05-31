ec90a68c5ce0cfc0758db975814e59cff22d6f4c
nakajimakotaro <harusann2@gmail.com>
2017-05-31 11:30:31 +0900

    バグだらけ

--- src/ink.ts
+++ src/ink.ts
@@ -1,47 +1,62 @@
 import PIXI = require('pixi.js');
 import Matter = require("matter-js");
-import {Game} from "./script";
-import {GameObject} from "./gameObject";
-import {Wall} from "./wall";
-import {Ball} from "./ball";
-import {Shape,Rectangle,Circle,Point} from "./shape";
-import {ViewObject} from "./viewObject";
+import { Game } from "./script";
+import { GameObject } from "./gameObject";
+import { Wall } from "./wall";
+import { Ball } from "./ball";
+import { Shape, Rectangle, Circle, Point } from "./shape";
+import { ViewObject } from "./viewObject";
 
-function random(min:number, max:number){
+function random(min: number, max: number) {
     return Math.random() * (max - min) + min;
 }
 
-export class Ink extends ViewObject{
-    view:PIXI.Graphics;
-    shape:Rectangle;
-    private body:Matter.Body;
-    public get position(){
-        return {x: this.body.position.x, y: this.body.position.y};
+export class Ink extends ViewObject {
+    view: PIXI.Graphics;
+    shape: Rectangle;
+    private sensor: Matter.Body;
+    public get position() {
+        return { x: this.sensor.position.x, y: this.sensor.position.y };
     }
-    public get angle(){
-        return this.body.angle;
+    public get angle() {
+        return this.sensor.angle;
     }
-    splashList: {shape:Circle, angle:number, speed:number, firstSpeed:{x:number, y:number}}[] = [];
-    constructor(public game:Game, public splashPoint:Point, public ball:Ball, wall:Wall, public color:number){
+    splashList: { shape: Circle, angle: number, speed: number, firstSpeed: { x: number, y: number } }[] = [];
+    constructor(public game: Game, private splashPoint: Point, private ball: Ball, private wall: Wall, public color: number) {
         super();
 
+
+        const range = Math.hypot(ball.sprite.x - wall.sprite.x, ball.sprite.y - wall.sprite.y);
+        const angle = Math.atan2(ball.sprite.y - wall.sprite.y, ball.sprite.x - wall.sprite.x);
+        //view
         this.view = new PIXI.Graphics();
+        this.game.level.view.addChild(this.view);
+        //sensor
+        this.sensor = Matter.Bodies.rectangle(wall.sprite.x + Math.cos(angle) * range, wall.sprite.y + Math.sin(angle) * range, 40, 40, { isSensor: true });
+        Matter.World.add(this.game.level.matterEngine.world, this.sensor);
+        const pin = Matter.Constraint.create({
+            bodyA: this.wall.body,
+            pointA: {x: this.sensor.position.x - this.wall.position.x, y: this.sensor.position.y - this.wall.position.y},
+            bodyB: this.sensor,
+            stiffness: 1,
+            length: 0
+        });
+        Matter.World.add(this.game.level.matterEngine.world, pin);
+        //mask
         const mask = new PIXI.Graphics();
-        this.view.mask = mask;
+        //this.view.mask = mask;
         mask.beginFill(0x00ffff);
         mask.drawRect(wall.sprite.width / -2, wall.sprite.height / -2, wall.sprite.width, wall.sprite.height);
         mask.position.x = wall.sprite.x;
         mask.position.y = wall.sprite.y;
         mask.rotation = wall.angle;
-        this.game.level.view.addChild(this.view);
-
 
         this.setShape(wall);
         this.splashCreate();
         this.splashDraw();
     }
 
-    setShape(wall:Wall){
+    setShape(wall: Wall) {
         //let x = this.splashPoint.x;
         //let y = this.splashPoint.y;
         //let width = 40;
@@ -72,25 +87,24 @@ export class Ink extends ViewObject{
         //this.shape = new Rectangle(x, y, width, height);
         //this.game.level.collision.add(this, this.shape);
     }
-    splashCreate(){
+    splashCreate() {
         const maxSize = 12;
         let inkVolume = 75;
-        while(inkVolume > 0){
+        while (inkVolume > 0) {
             let splashVolume = random(1, maxSize);
-            if(splashVolume < inkVolume){
+            if (splashVolume < inkVolume) {
                 inkVolume -= splashVolume;
-            }else{
+            } else {
                 splashVolume = inkVolume;
                 inkVolume = 0;
             }
             const shape = new Circle(
-                this.splashPoint.x,
-                this.splashPoint.y,
-                splashVolume
-            );
+                0,
+                0,
+                splashVolume);
             const angle = random(0, Math.PI * 2);
             const speed = (maxSize - shape.r) + 5;
-            const firstSpeed = {x: this.ball.moveAverage().x, y: this.ball.moveAverage().y};
+            const firstSpeed = { x: this.ball.moveAverage().x, y: this.ball.moveAverage().y };
 
             this.splashList.push(
                 {
@@ -101,9 +115,9 @@ export class Ink extends ViewObject{
                 });
         }
     }
-    splashDraw(){
+    splashDraw() {
         this.view.beginFill(this.color);
-        for(let splash of this.splashList){
+        for (let splash of this.splashList) {
             this.view.drawCircle(
                 splash.shape.x,
                 splash.shape.y,
@@ -112,21 +126,25 @@ export class Ink extends ViewObject{
         }
     }
     nextCount = 0;
-    splashNext(){
-        for(let splash of this.splashList){
+    splashNext() {
+        for (let splash of this.splashList) {
             splash.shape.x += Math.cos(splash.angle) * splash.speed + splash.firstSpeed.x;
             splash.shape.y += Math.sin(splash.angle) * splash.speed + splash.firstSpeed.y;
             splash.shape.r *= random(0.75, 0.95);
         }
         this.splashDraw();
     }
-    update(){
-        if(this.game.level.countFrame % 3 == 0 && this.nextCount < 4){
+    update() {
+        this.sensor.angle = this.wall.angle;
+        this.view.rotation = this.sensor.angle;
+        this.view.position.x = this.sensor.position.x;
+        this.view.position.y = this.sensor.position.y;
+        if (this.game.level.countFrame % 3 == 0 && this.nextCount < 4) {
             this.splashNext();
             this.nextCount++;
         }
         let debug = false;
-        if(debug){
+        if (debug) {
             this.game.level.view.endFill();
             this.game.level.view.lineStyle(1, 0x00ff00);
             this.game.level.view.moveTo(this.shape.left(), this.shape.top());
@@ -138,7 +156,7 @@ export class Ink extends ViewObject{
         }
     }
 
-    ability(ball:Ball, wall:Wall){
+    ability(ball: Ball, wall: Wall) {
         let xReverse = 1;
         let yReverse = 1;
         let angle = Math.atan2(ball.moveAverage().y * -1 * yReverse, ball.moveAverage().x * xReverse);
--- src/wall.ts
+++ src/wall.ts
@@ -6,7 +6,7 @@ import {ViewObject} from "./viewObject";
 import {Shape,Rectangle} from "./shape";;
 
 export class Wall extends ViewObject{
-    private body:Matter.Body;
+    public body:Matter.Body;
     public sprite:PIXI.Sprite;
     public get position(){
         return {x: this.body.position.x, y: this.body.position.y};
@@ -16,16 +16,20 @@ export class Wall extends ViewObject{
     }
     constructor(public game:Game, x:number, y:number, width:number, height:number){
         super();
+        //body
         this.body = Matter.Bodies.rectangle(x, y, width, height, {
             restitution: 0,
             friction: 0,
             frictionAir: 0,
             frictionStatic: 1,
         });
-        this.body.isStatic = true;
+        //this.body.isStatic = true;
         this.body["userObject"] = this;
         this.body["userCollisionList"] = [];
+        //this.body.torque = 10;
         Matter.World.add(this.game.level.matterEngine.world, this.body);
+        const pin = Matter.Constraint.create({bodyA: this.body, pointB: {x: this.body.position.x, y: this.body.position.y}, stiffness: 1})
+        Matter.World.add(this.game.level.matterEngine.world, pin);
         this.sprite = this.game.resourceLoader.rectangle(width, height, 0x9C27B0);
         this.game.level.view.addChild(this.sprite);
         this.sprite.x = x;
@@ -39,7 +43,11 @@ export class Wall extends ViewObject{
         if(angle > Math.PI * 2){
             angle -= Math.PI * 2;
         }
-        this.sprite.rotation = angle;
-        Matter.Body.rotate(this.body, 0.01)
+        this.sprite.position.x = this.body.position.x;
+        this.sprite.position.y = this.body.position.y;
+        this.sprite.rotation = this.body.angle;
+        //Matter.Body.rotate(this.body, 0.01);
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
     }
 }
